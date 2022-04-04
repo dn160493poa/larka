9a387c994689f58ad95a7e2f84e062b15a99dad6
Oleg Popkov <popkovoa@ektaservice.local>
2022-04-04 22:07:01 +0300

    updated 04/04/22 part 2

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -3,13 +3,18 @@
 
 namespace App\Http\Controllers\Post;
 
+use App\Http\Requests\Post\FilterRequest;
 use App\Models\Post;
 
 class IndexController extends BaseController
 {
-    public function __invoke()
+    public function __invoke(FilterRequest $request)
     {
+
+        //$data = $request->validated();
+
         $posts = Post::paginate(10);
+
 
         return view('posts.index', compact('posts'));
     }
--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::group(['namespace' => 'Post'], function () {
     Route::get('/posts', 'IndexController')->name('post.index');
     Route::get('/posts/create', 'CreateController')->name('post.create');
 
-    Route::post('/posts', 'StoreController')->name('post.store');
+    Route::post('/posts', 'StoreController')->name('post.store'); //
     Route::get('/posts/{post}', 'ShowController')->name('post.show');
     Route::get('/posts/{post}/edit', 'EditController')->name('post.edit');
     Route::patch('/posts/{post}', 'UpdateController')->name('post.update');
